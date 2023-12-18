25d39e67fdca8863944e2b034f738a5cff39f970
dilmurod1701 <dilmurodesonboev1701@gmail.com>
2023-12-18 22:13:58 +0500

    Added: Question table

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Question(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    hashtag = models.CharField(max_length=50)
+    likes = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
