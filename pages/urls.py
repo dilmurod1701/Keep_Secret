1951ca5790af9a99b6833ace4e4d67e27d3e1464
dilmurod1701 <dilmurodesonboev1701@gmail.com>
2023-12-18 22:23:34 +0500

    Added: pathes

---
+++ pages/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('ques', views.all_questions, name='question'),
+    path('add', views.CreateQuestion.as_view(), name='add'),
+    path('ques/<int:pk>', views.DetailQuestion.as_view(), name='detail'),
+    path('ques/<int:pk>/add-comment', views.add_comment, name='add-comment'),
+    path('search/', views.search, name='search'),
+]
