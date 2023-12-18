773983eb2fa7b160f6624e3f64f9c30c4976005e
dilmurod1701 <dilmurodesonboev1701@gmail.com>
2023-12-18 22:37:04 +0500

    Added: Profile table

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    img = models.ImageField(upload_to='profile_images', default='profile.png')
+
+    class Meta:
+        db_table = 'profile'
+
+    def __str__(self):
+        return f"{self.user.username}"
