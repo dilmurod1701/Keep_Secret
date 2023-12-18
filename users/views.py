9a851aa96b6ee725f77f6444db4bdc0633cf7cfd
dilmurod1701 <dilmurodesonboev1701@gmail.com>
2023-12-18 22:40:05 +0500

    Added: Login class to login users

--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .forms import SignUp
+
 
 # Create your views here.
+class Login(LoginView):
+    template_name = 'users/login.html'
+    next_page = 'question'
