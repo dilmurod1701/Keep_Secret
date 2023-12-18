adb4ca0f5b6850969cde76ad33a49b1e6baecb2d
dilmurod1701 <dilmurodesonboev1701@gmail.com>
2023-12-18 22:16:28 +0500

    Added: CommentForm

--- pages/forms.py
+++ pages/forms.py
@@ -7,3 +7,9 @@ class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question
         fields = ['user', 'text', 'hashtag']
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['text']
