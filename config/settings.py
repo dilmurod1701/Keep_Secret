d0c42c7a5e03872e3fd97d013a2a6dd973fc714a
dilmurod <139139677+dilmurod1701@users.noreply.github.com>
2023-12-19 22:14:15 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ TEMPLATES_DIR = BASE_DIR / 'templates'
 SECRET_KEY = 'django-insecure-gxor=bmfdof$9%*xt8=&4!2oo2w^=*+a_z9yjg0cxu+7_ig)=1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*', '127.0.0.1', '.vercel.app']
 
