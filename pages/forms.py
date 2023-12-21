b14ad9a204412ee263febaebb6e9613939711e9b
dilmurod <139139677+dilmurod1701@users.noreply.github.com>
2023-12-21 14:02:54 +0500

    Updated: forms.py

--- pages/forms.py
+++ pages/forms.py
@@ -6,7 +6,7 @@ from .models import Question, Comment
 class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question
-        fields = ['user', 'text', 'hashtag']
+        fields = ['text', 'hashtag']
 
 
 class CommentForm(forms.ModelForm):
